e8b42d139971f71bf2979c1abb50459eb1bb58d6
Ismael Shabazz Issaka <ismael.issaka@etu.unige.ch>
2026-01-17 00:29:45 +0100

    added routing to the page, not working yet but not broken either; next: required components for the missing links.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,71 +1,21 @@
+import { BrowserRouter, Routes, Route, useLocation, useNavigate } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ticketData from './assets/tickets.json'
+import TicketList from './components/TicketList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Support Tickets</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        
-    <div className="controls">
-        <label>Filter by Status:
-            <select>
-                <option value="all">All</option>
-                <option value="open">Open</option>
-                <option value="in_progress">In Progress</option>
-                <option value="closed">Closed</option>
-            </select>
-        </label>
-
-        <label>Sort by:
-            <select>
-                <option value="created_at">Date Created</option>
-                <option value="title">Title</option>
-            </select>
-        </label>
-
-        <a href="tickets/new.html" className="btn-new">+ New Ticket</a>
-    </div>
-
-    <table>
-        <thead>
-            <tr>
-                <th>Title (Click to expand)</th>
-                <th>Created At</th>
-                <th>Status</th>
-            </tr>
-        </thead>
-        <tbody>
-            { ticketData.map(ticket => (
-                <tr key={ticket.id}>
-                    <td><a href={`tickets/${ticket.id}`}>{ticket.title}</a></td>
-                    <td>{ticket.created_at}</td>
-                    <td><span className="status-pill">{ticket.status}</span></td>
-                </tr>
-            )) }
-
-        </tbody>
-    </table>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TicketList />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/components/TicketList.tsx
@@ -0,0 +1,65 @@
+import TicketData from '../assets/tickets.json'
+import { Link, Outlet } from 'react-router-dom';
+
+
+
+
+
+export default function TicketList() {
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Support Tickets</h1>
+      
+
+
+        <div className="controls">
+          <label>{` Filter by Status: `}
+              <select>
+                  <option value="all">All</option>
+                  <option value="open">Open</option>
+                  <option value="in_progress">In Progress</option>
+                  <option value="closed">Closed</option>
+              </select>
+          </label>
+          <label>{` Sort by: `} 
+              <select>
+                  <option value="created_at">Date Created</option>
+                  <option value="title">Title</option>
+              </select>
+          </label>
+          &nbsp; 
+          <Link to="/tickets/new" style={btnStyle}>+ New Ticket    </Link>
+      </div>
+
+          <br></br> 
+      
+
+      <table>
+      <thead>
+        <tr>
+            <th>Title (Click to expand)</th>
+            <th>Created At</th>
+            <th>Status</th>
+        </tr>
+      </thead>
+        {/* Render your table rows here */}
+        <tbody>
+        {TicketData.map(t => (
+          <tr key={t.id}>
+            <td><Link to={`/tickets/${t.id}`}>{t.title}</Link></td>
+            <td>{t.created_at}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+      {/* This renders the Modal when the path is /tickets/new or /tickets/:id */}
+      <Outlet />
+    </div>
+  );
+}
+
+const btnStyle = { background: '#638cebff', color: 'white', padding: '10px', textDecoration: 'none' };
+
+
